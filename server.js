21314e8a6e9a1b5218b8d742e44e5d72ead9b021
spanempty <125310755+spanempty@users.noreply.github.com>
2024-08-14 21:46:05 +0300

    only the home page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const app = express();
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
